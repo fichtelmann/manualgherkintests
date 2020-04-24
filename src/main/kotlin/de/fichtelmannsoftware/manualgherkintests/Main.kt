a9028bda56b13247c8190a0dc562fbd9f958b5f4
Kay Fichtelmann <info@fichtelmann-software.de>
2020-04-24 23:21:08 +0200

    Initial commit Gradle Project

---
+++ src/main/kotlin/de/fichtelmannsoftware/manualgherkintests/Main.kt
@@ -0,0 +1,11 @@
+package de.fichtelmannsoftware.manualgherkintests
+fun main(args: Array<String>) {
+    welcome()
+    println(args[0])
+}
+
+fun welcome() {
+    println("########\tManual Gherkin Tests\t########")
+}
+
+
