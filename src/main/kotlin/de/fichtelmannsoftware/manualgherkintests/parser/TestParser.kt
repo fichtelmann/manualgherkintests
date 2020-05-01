29219d21d27141855c22385c34db5cf4c6aada9d
Kay Fichtelmann <info@fichtelmann-software.de>
2020-05-01 20:11:07 +0200

    Using gherkin library for parsing feature file

--- src/main/kotlin/de/fichtelmannsoftware/manualgherkintests/parser/TestParser.kt
+++ src/main/kotlin/de/fichtelmannsoftware/manualgherkintests/parser/TestParser.kt
@@ -1,10 +1,16 @@
 package de.fichtelmannsoftware.manualgherkintests.parser
 
 import de.fichtelmannsoftware.manualgherkintests.poko.ManualTest
-import de.fichtelmannsoftware.manualgherkintests.poko.ManualTestCase
+import io.cucumber.gherkin.Gherkin
+import io.cucumber.messages.IdGenerator
+import io.cucumber.messages.Messages
+import io.cucumber.messages.Messages.GherkinDocument
 import java.io.File
+import java.util.stream.Collectors
+
 
 class TestParser(path: File) {
+    val idGenerator = IdGenerator.Incrementing()
     var manualTest = ManualTest()
 
     enum class TestStepType {
@@ -23,61 +29,25 @@ class TestParser(path: File) {
     }
 
     private fun parseFeatureFile(featureFile: File) {
-        val testCases = mutableListOf<ManualTestCase>()
-        var actualTestCase = ManualTestCase()
+        val paths = listOf(featureFile.absolutePath)
+        val includeSource = false
+        val includeAst = true
+        val includePickles = true
+        val envelopes =
+            Gherkin.fromPaths(paths, includeSource, includeAst, includePickles, idGenerator)
+                .collect(
+                    Collectors.toList<Messages.Envelope>()
+                )
+        // Get the AST
+        val gherkinDocument: GherkinDocument = envelopes[0].gherkinDocument
 
-        featureFile.forEachLine {
-            when (identifyType(it)) {
-                TestStepType.Feature -> actualTestCase.feature += prepareString(it, TestStepType.Feature)
-                TestStepType.Description -> actualTestCase.description += prepareString(it, TestStepType.Description)
-                TestStepType.Preparation -> actualTestCase.preparation += prepareString(it, TestStepType.Preparation)
-                TestStepType.Action -> actualTestCase.actions += prepareString(it, TestStepType.Action)
-                TestStepType.Expected -> actualTestCase.expectedResult += prepareString(it, TestStepType.Expected)
-                else -> println(it)
-            }
-        }
-        println(actualTestCase.feature)
-        println(actualTestCase.actions)
-    }
+        // Get the Feature node of the AST
+        val feature = gherkinDocument.feature
+        println("Feature: $feature")
 
-    /**
-     * Trim the string search for the particular keyword and reduce the beginning of the string.
-     * If a colon is near the keyword it will be removed also.
-     */
-    private fun prepareString(actualLine: String, testStepType: TestStepType): String {
-        var newLine = actualLine.trim()
-        var position = when (testStepType) {
-            TestStepType.Feature -> STRING_FEATURE.length
-            TestStepType.Description -> STRING_SCENARIO.length
-            TestStepType.Preparation -> STRING_WHEN.length
-            TestStepType.Action -> STRING_GIVEN.length
-            TestStepType.Expected -> STRING_THEN.length
-            else -> INVALID_POSITION
-        }
-
-        if (position != INVALID_POSITION) {
-            val indexColon = actualLine.indexOf(":")
-            if (indexColon != INVALID_POSITION && (indexColon == position || indexColon == position + 1)) {
-                position = indexColon + 1
-            }
-            newLine = newLine.subSequence(position, newLine.length).toString()
-        }
-        return newLine.trim()
-    }
-
-    /**
-     * Check if a particular string is inside the given line and
-     * @return the matching TestStepType.
-     */
-    private fun identifyType(line: String): TestStepType {
-        return when {
-            line.trim().startsWith(STRING_FEATURE, ignoreCase = true) -> TestStepType.Feature
-            line.trim().startsWith(STRING_SCENARIO, ignoreCase = true) -> TestStepType.Description
-            line.trim().startsWith(STRING_GIVEN, ignoreCase = true) -> TestStepType.Preparation
-            line.trim().startsWith(STRING_WHEN, ignoreCase = true) -> TestStepType.Action
-            line.trim().startsWith(STRING_THEN, ignoreCase = true) -> TestStepType.Expected
-            else -> TestStepType.Unknown
-        }
+        // Get the first Scenario node of the Feature node
+        val scenario = feature.getChildren(0).scenario
+        println("Scenario: $scenario")
     }
 
     companion object {
